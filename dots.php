38ede463e881fe922565fefe070f9b08d8da8e12
Xiaohua <xguan@salesforce.com>
2018-01-29 16:06:40 -0800

    plotting the flight paths

--- dots.php
+++ dots.php
@@ -1,6 +1,13 @@
 <?php
 require("phpsqlajax_dbinfo.php");
 
+//phpsqlajax_dbinfo.php has the following info
+
+//$username="somedbuser";
+//$password="somedbuser";
+//$database="somedb";
+
+
 function parseToXML($htmlStr)
 {
 $xmlStr=str_replace('<','&lt;',$htmlStr);
@@ -18,27 +25,45 @@ if (!$mysqli) {
 }
 
 // Select all the rows in the markers table
-$query = "SELECT * FROM F2006 limit 10";
-//DATE_TIME_UTC>=DATE('2016-12-01') AND DATE_TIME_UTC<DATE('2016-12-02') ";
+// $query = "SELECT * FROM F2006 limit 100";
+$query = "select * from F2006 where DATE_TIME_UTC > '2006-12-10' and DATE_TIME_UTC < '2006-12-11' order by  FLIGHT_INDEX,DATE_TIME_UTC  limit 100";
+
 $result = $mysqli->query($query);
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
 
 header("Content-type: text/xml");
-
+$lastFlight = "null";
 // Start XML file, echo parent node
-echo '<markers>';
+echo "<markers>\r\n";
 
 // Iterate through the rows, printing XML nodes for each
 while ($row = mysqli_fetch_assoc($result)){
+  // check if we need to create a new path
   // Add to XML document node
+  if ($lastFlight != $row['FLIGHT_INDEX']) {
+     if($lastFlight != 'null') {
+       // close flight
+       echo "</flight>\r\n";
+     }
+    $lastFlight = $row['FLIGHT_INDEX'];
+     // start a new marker
+    echo '<flight> ';
+  }
   echo '<marker ';
   echo 'flight="' . parseToXML($row['AIRCRAFT_ID']) . '" ';
+  
   echo 'lat="' . $row['LATITUDE'] . '" ';
   echo 'lng="' . $row['LONGITUDE'] . '" ';
   echo 'id="' . $row['ID'] . '" ';
-  echo '/>';
+  echo "/>\r\n";
+}
+
+// last closing flight
+if($lastFlight != 'null') {
+  // close marker
+  echo "</flight>\r\n";
 }
 
 // End XML file
