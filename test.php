fce008d7667cd3f201e45f9efb16ef49ffe544dc
Xiaohua <xguan@salesforce.com>
2018-02-14 09:44:12 -0800

    removing test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqlhost="cv5286.myfoscam.org";
-#$mysqlhost="localhost";
-$username="dbuser";
-$password="dbuser";
-$database="flightdb";
-$db_connection = pg_connect("host=".$mysqlhost." dbname=".$database." user=".$username." password=".$password);
-
-?>
